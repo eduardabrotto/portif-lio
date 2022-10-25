510240cdcf8222c1d82c616a96cc735df78e92c0
eduarda <eduardabrotto@outlook.com>
2022-10-25 20:32:32 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1. Armazene na variável nome colaboradora o nome Maria e em seguida utilize o console.log() para ver o valor;
+//2. Armazene uma soma entre dois números e mostre no console.log o resultado obtido;
+//3. Armazene separadamente, TODOS os tipos de dados que vocês aprenderam até o momento; Utilize o console.log(typeof(nomeDaVariavel)) para verificar que interessante.
+//4. Armazene o nome chamado Eduarda em uma variável e use a notação de colchete para encontrar o primeiro caractere do nome armazenado. 
+//    Por exemplo, o primeiro caractere do nome Eduarda é E. No caso, no console.log() deverá aparecer SOMENTE A PRIMEIRA LETRA.
+
+const nomeColaboradora = "maria"
+console.log(nomeColaboradora)
+
+const a = 5
+const b = 3
+const obterResultadoSoma = a + b
+console.log(obterResultadoSoma)
+
+const string = "nome string"
+const inteiro = 11
+const float = 11.3
+const booleanoTrue = true
+const booleanoFalse = false
+
+console.log(nomeColaboradora.charAt(0))
+
+//Crie duas variáveis e compare os valores: 
+//1. a variável x é maior que o variável y? 
+//Se for verdadeira, retornar com a mensagem no console.log(O maior número é x). 
+//Se não for verdadeira, retornar com a mensagem no console.log(O número maior é y);
+
+
